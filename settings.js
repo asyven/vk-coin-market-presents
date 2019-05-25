e8c12cd37f76b376c8ce9dbedf63783418713ead
asyven <asyven3@gmail.com>
2019-05-26 01:02:35 +0300

    upd

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 module.exports = {
     VK_ID: 541198358, //id пользователя
     VK_TOKEN: "123", //расширеный токен для вк
-    VKCOIN_MERCHANT: "123", //токен самого вк коина
-    VK_WIDGET_TOKEN: "123", //merchant api vk coin
+    VKCOIN_MERCHANT: "123", //merchant api vk coin
+    VK_WIDGET_TOKEN: "123", //токен виджета сообщества
 
     GROUP: 'aionpwcoin', //группа
 
@@ -11,24 +11,3 @@ module.exports = {
     GROUP_STAND_IGNORE: [69669411],//пользователи, которые не должны побеждать (админы, модеры и тп.)
     GROUP_STAND_REWARD: 5000, //потом умножится на 1000
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
